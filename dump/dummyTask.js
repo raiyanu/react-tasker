fab5338a4a763ea0c6ccff6029d445a556a1a1bc
Raiyan Ahmed <ray38250968@gmail.com>
2023-12-20 21:29:16 +0530

    chores

---
+++ dump/dummyTask.js
@@ -0,0 +1,62 @@
+const data = [
+    {
+        title: "Buy groceries",
+        dateCreated: new Date("2023-01-15T09:00:00"),
+        reminderDate: new Date("2023-01-18T18:00:00"),
+        completed: false
+    },
+    {
+        title: "Read a book",
+        dateCreated: new Date("2023-02-01T12:30:00"),
+        reminderDate: new Date("2023-02-10T15:00:00"),
+        completed: true,
+    },
+    {
+        title: "Write a blog post",
+        dateCreated: new Date("2023-03-05T10:45:00"),
+        reminderDate: new Date("2023-03-15T14:30:00"),
+        completed: false,
+    },
+    {
+        title: "Exercise",
+        dateCreated: new Date("2023-04-10T08:00:00"),
+        reminderDate: new Date("2023-04-15T17:30:00"),
+        completed: false,
+    },
+    {
+        title: "Learn React",
+        dateCreated: new Date("2023-05-20T14:00:00"),
+        reminderDate: new Date("2023-06-01T10:00:00"),
+        completed: false,
+    },
+    {
+        title: "Watch a movie",
+        dateCreated: new Date("2023-07-08T20:15:00"),
+        reminderDate: new Date("2023-07-15T19:00:00"),
+        completed: true,
+    },
+    {
+        title: "Plan vacation",
+        dateCreated: new Date("2023-08-25T11:30:00"),
+        reminderDate: new Date("2023-09-05T09:45:00"),
+        completed: false,
+    },
+    {
+        title: "Cook dinner",
+        dateCreated: new Date("2023-10-12T18:30:00"),
+        reminderDate: new Date("2023-10-20T17:00:00"),
+        completed: false,
+    },
+    {
+        title: "Attend meeting",
+        dateCreated: new Date("2023-11-02T15:45:00"),
+        reminderDate: new Date("2023-11-10T13:30:00"),
+        completed: true,
+    },
+    {
+        title: "Read articles",
+        dateCreated: new Date("2023-12-15T09:30:00"),
+        reminderDate: new Date("2023-12-20T11:00:00"),
+        completed: false,
+    },
+]
